45666c15ab694c8ba3c9d156d7d657aa943a9943
Evan Murphy <esmurphy7@gmail.com>
2025-09-21 12:34:31 -0700

    part 1 of codesignal banking system

---
+++ codesignal/banking-system/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var solution = new Solution();
+
+        // =====================
+        // Part 1
+        // =====================
+        solution.CreateAccount(1, "account1");
+        solution.CreateAccount(2, "account1");
+        solution.CreateAccount(3, "account2");
+        solution.PrintAll();
+
+        solution.Deposit(4, "non-existing", 2700);
+        solution.PrintAll();
+
+        solution.Deposit(5, "account1", 2700);
+        solution.PrintAll();
+
+        solution.Transfer(6, "account1", "account2", 200);
+        solution.PrintAll();        
+    }
+}
---
+++ codesignal/banking-system/Solution.cs
@@ -0,0 +1,151 @@
+using System.Transactions;
+
+public class Solution
+{
+    public Dictionary<string, Account> AccountsbyId { get; set; } = new Dictionary<string, Account>();
+    public Dictionary<string, List<Transaction>> TxsBySrcAccountId { get; set; } = new Dictionary<string, List<Transaction>>();
+
+    public bool CreateAccount(int timestamp, string accountId)
+    {
+        if (this.AccountsbyId.TryGetValue(accountId, out var _))
+        {
+            return false;
+        }
+
+        this.AccountsbyId[accountId] = new Account
+        {
+            Id = accountId,
+            Balance = 0,
+        };
+
+        this.TxsBySrcAccountId[accountId] = new List<Transaction>();
+
+        return true;
+    }
+
+    public int? Deposit(int timestamp, string accountId, int amount)
+    {
+        if (this.AccountsbyId.TryGetValue(accountId, out var account))
+        {
+            account.Balance += amount;
+
+            this.TxsBySrcAccountId[accountId].Add(new Transaction
+            {
+                Timestamp = timestamp,
+                DestAccount = accountId,
+                Type = TransactionType.Deposit,
+            });
+
+            return account.Balance;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public int? Transfer(int timestamp, string srcAccountId, string destAccountId, int amount)
+    {
+        if (srcAccountId == destAccountId)
+        {
+            return null;
+        }
+
+        if (this.AccountsbyId.TryGetValue(srcAccountId, out var srcAccount))
+        {
+            if ((srcAccount.Balance - amount) < 0)
+            {
+                return null;
+            }
+
+            if (this.AccountsbyId.TryGetValue(destAccountId, out var destAccount))
+            {
+                srcAccount.Balance -= amount;
+                destAccount.Balance += amount;
+
+                this.TxsBySrcAccountId[srcAccountId].Add(new Transaction
+                {
+                    Timestamp = timestamp,
+                    SrcAccount = srcAccountId,
+                    DestAccount = destAccountId,
+                    Type = TransactionType.Transfer,
+                });
+
+                return srcAccount.Balance;
+            }
+        }
+
+        return null;
+    }
+
+    public void PrintAll()
+    {
+        Console.WriteLine($"__________");
+        this.PrintAccounts();
+        this.PrintTransactions();
+    }
+
+    public void PrintAccounts()
+    {
+        Console.WriteLine($"Accounts ({this.AccountsbyId.Keys.Count}) ======");
+
+        foreach (var accountId in this.AccountsbyId.Keys)
+        {
+            this.PrintAccount(accountId);
+        }
+    }
+
+    public void PrintAccount(string accountId)
+    {
+        if (this.AccountsbyId.TryGetValue(accountId, out var account))
+        {
+            Console.WriteLine($"account {account.Id} ------");
+            Console.WriteLine($"balance: {account.Balance}");
+        }
+    }
+
+    public void PrintTransactions()
+    {
+        foreach (var accountId in this.TxsBySrcAccountId.Keys)
+        {
+            this.PrintTransactions(accountId);
+        }
+    }
+
+    public void PrintTransactions(string accountId)
+    {
+        if (this.AccountsbyId.TryGetValue(accountId, out var _))
+        {
+            Console.WriteLine($"account {accountId}'s txs ({this.TxsBySrcAccountId[accountId].Count}) ======");
+
+            foreach (var tx in this.TxsBySrcAccountId[accountId])
+            {
+                Console.WriteLine($"tx at {tx.Timestamp} --------");
+                Console.WriteLine($"timestamp: {tx.Timestamp}");
+                Console.WriteLine($"srcAccount: {tx.SrcAccount}");
+                Console.WriteLine($"destAccount: {tx.DestAccount}");
+                Console.WriteLine($"typ: {tx.Type}");
+            }
+        }
+    }
+}
+
+public class Account
+{
+    public string Id { get; set; }
+    public int Balance { get; set; }
+}
+
+public class Transaction
+{
+    public int Timestamp { get; set; }
+    public string SrcAccount { get; set; }
+    public string DestAccount { get; set; }
+    public TransactionType Type { get; set; }
+}
+
+public enum TransactionType
+{
+    Deposit,
+    Transfer
+}
